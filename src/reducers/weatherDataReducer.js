5faef67b7c34a6ccd50976c387dbf2dc6891190d
Madhavi A <39688254+madhavi506@users.noreply.github.com>
2021-07-29 01:34:12 +0530

    Update weatherDataReducer.js

--- src/reducers/weatherDataReducer.js
+++ src/reducers/weatherDataReducer.js
@@ -2,18 +2,15 @@ const initialState = {
     weatherData: [],
     loading: true
   };
-  // Redux: Counter Reducer
   const weatherDataReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_WEATHER_DATA': {
-          console.log(action, 'GET_WEATHER_DATA')
             return {
               ...state,
               loading: true,
             };
           }
       case 'GET_WEATHER_DATA': {
-        console.log(action, 'GET_WEATHER_DATA')
         return {
           ...state,
           weatherData: action.action,
